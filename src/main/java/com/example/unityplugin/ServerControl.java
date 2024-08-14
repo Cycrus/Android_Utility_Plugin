0b851505780d90576432f1cf5ea5888dfc70ab39
Cycrus <marxcyril@gmail.com>
2024-08-14 21:11:18 +0200

    Implemented automatic pingin of server to evaluate if server is still on. Also refactored addresses to be passed as initialization argument.

--- src/main/java/com/example/unityplugin/ServerControl.java
+++ src/main/java/com/example/unityplugin/ServerControl.java
@@ -1,6 +1,7 @@
 package com.example.unityplugin;
 
 import android.app.Activity;
+import android.view.KeyEvent;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -12,21 +13,39 @@ import java.net.SocketException;
 
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
 import java.io.ByteArrayOutputStream;
+import java.net.UnknownHostException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ServerControl {
     private final int port = 9;
     private DatagramSocket serverManager;
     private static Activity unityActivity;
+    private boolean serverReachable = false;
+    private boolean networkReachable = false;
+    private int pingTimeout = 1000;
+    private String gatewayAddress;
+    private String mainServerAddress;
+    private String wolRouterAddress;
+    private String mainServerMac;
 
     public static void receiveUnityActivity(Activity tActivity) {
         unityActivity = tActivity;
     }
 
-    public void initializeModule() throws SocketException {
+    public void initializeModule(String gatewayAddress, String mainServerAddress,
+                                 String wolRouterAddress, String mainServerMac) throws SocketException {
+        this.gatewayAddress = gatewayAddress;
+        this.mainServerAddress = mainServerAddress;
+        this.wolRouterAddress = wolRouterAddress;
+        this.mainServerMac = mainServerMac;
         serverManager = new DatagramSocket();
+        startPingWorkers();
     }
 
     private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
@@ -65,61 +84,101 @@ public class ServerControl {
     }
 
     public void sendMacOverTcp(String macAddress, String address, int port) throws IOException {
-        Socket tcpSocket = new Socket(address, port);
-        OutputStream msgOutput = tcpSocket.getOutputStream();
-        msgOutput.write(macAddress.getBytes());
-        msgOutput.flush();
-        tcpSocket.close();
+        Thread thread = new Thread(() -> {
+            try {
+                Socket tcpSocket = new Socket(address, port);
+                OutputStream msgOutput = tcpSocket.getOutputStream();
+                msgOutput.write(macAddress.getBytes());
+                msgOutput.flush();
+                tcpSocket.close();
+            } catch (IOException e) {
+                System.err.println("[Error] Cannot send TCP message. " + e.toString());
+            }
+        });
+        thread.start();
     }
 
-    public static String executeRemoteCommand(
+    public static void executeRemoteCommand(
             String username,
             String password,
             String hostname,
             int port,
             String command) throws Exception {
 
-        JSch jsch = new JSch();
-        Session session = jsch.getSession(username, hostname, port);
-        session.setPassword(password);
+        Thread thread = new Thread(() -> {
+            try {
+                JSch jsch = new JSch();
+                Session session = jsch.getSession(username, hostname, port);
+                session.setPassword(password);
 
-        // Avoid asking for key confirmation
-        java.util.Properties config = new java.util.Properties();
-        config.put("StrictHostKeyChecking", "no");
-        session.setConfig(config);
+                // Avoid asking for key confirmation
+                java.util.Properties config = new java.util.Properties();
+                config.put("StrictHostKeyChecking", "no");
+                session.setConfig(config);
 
-        session.connect();
+                session.connect(5000);
 
-        // SSH Channel
-        ChannelExec channelExec = (ChannelExec) session.openChannel("exec");
-        ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
-        channelExec.setOutputStream(responseStream);
+                // SSH Channel
+                ChannelExec channelExec = (ChannelExec) session.openChannel("exec");
+                ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+                channelExec.setOutputStream(responseStream);
 
-        // Execute command
-        channelExec.setCommand(command);
-        channelExec.connect();
+                // Execute command
+                channelExec.setCommand(command);
+                channelExec.connect();
 
-        // Read the output from the command
-        while (channelExec.isConnected()) {
-            Thread.sleep(100);
-        }
-
-        String responseString = responseStream.toString();
-
-        // Disconnect the channel and session
-        channelExec.disconnect();
-        session.disconnect();
-
-        return responseString;
+                // Disconnect the channel and session
+                channelExec.disconnect();
+                session.disconnect();
+            } catch (JSchException e) {
+                System.err.println("[Error] Cannot send SSH command. " + e.toString());
+            }
+        });
+        thread.start();
     }
 
     public void startServer() throws IOException {
-        //sendWakeOnLan("FC:34:97:A5:F1:3C", "192.168.50.255");
-        sendMacOverTcp("FC:34:97:A5:F1:3C", "192.168.50.77", 3001);
+        sendMacOverTcp(mainServerMac, wolRouterAddress, 3001);
     }
 
     public void shutdownServer(String username, String password, String hostname) throws Exception {
         executeRemoteCommand(username, password, hostname,
         22, "sudo shutdown now");
+    }
+
+    public boolean isNetworkReachable() {
+        return networkReachable;
+    }
+
+    public boolean isServerReachable() {
+        return serverReachable;
+    }
+
+    private boolean isAddressReachable(String address) {
+        try {
+            InetAddress inet = InetAddress.getByName(address);
+            return inet.isReachable(pingTimeout);
+        } catch (IOException e) {
+            System.out.println("[Ping] Cannot ping address. " + e.toString());
+            return false;
+        }
+    }
+
+    private void startPingWorkers() {
+        System.out.println("[Ping] Starting network ping thread.");
+        ScheduledExecutorService networkPingScheduler = Executors.newScheduledThreadPool(1);
+        Runnable networkPingTask = () -> {
+            networkReachable = isAddressReachable(gatewayAddress);
+        };
+        networkPingScheduler.scheduleAtFixedRate(networkPingTask, 0, pingTimeout, TimeUnit.MILLISECONDS);
+
+        System.out.println("[Ping] Starting server ping thread.");
+        ScheduledExecutorService serverPingScheduler = Executors.newScheduledThreadPool(1);
+        Runnable serverPingTask = () -> {
+            if(networkReachable) {
+                serverReachable = isAddressReachable(mainServerAddress);
+            }
+        };
+        serverPingScheduler.scheduleAtFixedRate(serverPingTask, 0, pingTimeout, TimeUnit.MILLISECONDS);
     }
 }
